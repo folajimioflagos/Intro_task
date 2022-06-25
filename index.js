f9292a6daea86de116845c369c6cb0b6b223e5cf
folajimioflagos <folarh.j@gmail.com>
2022-06-25 15:47:36 -0700

    task code was inserted

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log(`Welcome`)
+// alert(`Hello World`);
+var firstName = "Folajimi";
+var surname = "Ayodele";
+var height = "5`11";
+var country = "Nigeria";
+
+console.log(" My name is " + firstName + " I am " + height + " and I am from " + country);
